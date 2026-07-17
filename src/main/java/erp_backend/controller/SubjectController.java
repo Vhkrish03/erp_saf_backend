c2ad2583ef93c8b1dba7ad9b554fe260df1f178b
Vhkrish03 <vhkrish03@gmail.com>
2026-07-18 00:38:53 +0530

    Added Subject API_01

---
+++ src/main/java/erp_backend/controller/SubjectController.java
@@ -0,0 +1,25 @@
+package erp_backend.controller;
+
+import java.util.List;
+
+import javax.security.auth.Subject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import erp_backend.service.SubjectService;
+
+@RestController
+@RequestMapping("/api/subjects")
+public class SubjectController {
+
+    @Autowired
+    private SubjectService service;
+
+    @GetMapping
+    public List<Subject> getSubjects() {
+        return service.getAllSubjects();
+    }
+}
