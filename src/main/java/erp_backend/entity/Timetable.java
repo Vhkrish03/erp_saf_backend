1ae5040ecd18b0575c744398f7049b4aa3a2bf4e
Vhkrish03 <vhkrish03@gmail.com>
2026-07-19 13:31:26 +0530

    updated timetable file

--- src/main/java/erp_backend/entity/Timetable.java
+++ src/main/java/erp_backend/entity/Timetable.java
@@ -27,5 +27,66 @@ public class Timetable {
     public Timetable() {
     }
 
-    // Getters and Setters
+    public Timetable(Long id, String day, String time, String subject, String room, String faculty) {
+        this.id = id;
+        this.day = day;
+        this.time = time;
+        this.subject = subject;
+        this.room = room;
+        this.faculty = faculty;
+    }
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for day
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    // Getter and Setter for time
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    // Getter and Setter for subject
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    // Getter and Setter for room
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    // Getter and Setter for faculty
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
 }
