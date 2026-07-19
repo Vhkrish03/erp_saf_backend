bb2167ceec523a1c69a909681029d6a110fb8fb7
Vhkrish03 <vhkrish03@gmail.com>
2026-07-19 13:44:28 +0530

    updated timetablecontroller

--- src/main/java/erp_backend/controller/TimetableController.java
+++ src/main/java/erp_backend/controller/TimetableController.java
@@ -2,7 +2,11 @@ package erp_backend.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import erp_backend.entity.Timetable;
 import erp_backend.service.TimetableService;
@@ -27,4 +31,22 @@ public class TimetableController {
     public List<Timetable> getByDay(@PathVariable String day) {
         return service.getByDay(day);
     }
+
+
+    @GetMapping("/test01")
+    public Timetable test() {
+    
+        Timetable t = new Timetable();
+    
+        t.setId(1L);
+        t.setDay("Monday");
+        t.setTime("9:00 - 9:50");
+        t.setSubject("Operating Systems");
+        t.setRoom("A-201");
+        t.setFaculty("Dr. Meera");
+    
+        return t;
+    }
+
+    
 }
