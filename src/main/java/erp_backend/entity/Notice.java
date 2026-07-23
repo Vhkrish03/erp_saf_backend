7d298d341505809fb8c1837c1b9dfbf99261f61e
Vhkrish03 <vhkrish03@gmail.com>
2026-07-23 22:25:40 +0530

    updated and added the notice entity file

--- src/main/java/erp_backend/entity/Notice.java
+++ src/main/java/erp_backend/entity/Notice.java
@@ -24,7 +24,61 @@ public class Notice {
 
     private String category;
 
+    @Column(name = "is_important")
     private boolean isImportant;
 
-    // Getters and Setters
+    public Notice() {
+    }
+
+    // Getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isImportant() {
+        return isImportant;
+    }
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setImportant(boolean isImportant) {
+        this.isImportant = isImportant;
+    }
 }
